f77a4605f21952c17ed7121c6580a1502bc0ea94
Dizy <dev@dizy.cc>
2022-02-13 18:44:27 -0500

    Fix headless value

--- src/@env.ts
+++ src/@env.ts
@@ -1,7 +1,9 @@
 export const NET_ID = process.env.NET_ID!;
 export const NET_ID_PASSWORD = process.env.NET_ID_PASSWORD!;
 
-export const HEADLESS = process.env.HEADLESS === 'true';
+export const HEADLESS = !(
+  !process.env.HEADLESS || process.env.HEADLESS.toLowerCase() === 'false'
+);
 
 export const DUO_AUTH_MANUAL =
   process.env.DUO_AUTH_MANUAL &&
